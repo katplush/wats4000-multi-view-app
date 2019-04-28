e78e62ae828e5a7f4007c7cb80176257af42816d
Katerina Plushko <katplush@yahoo.com>
2019-04-27 18:23:36 -0700

    added code to build

---
+++ src/views/Secret.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="component">
+    <h2>{{ message }}</h2>
+  </div>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        message: 'The key to pursuing excellence is to embrace an organic long term learning process, and not live in a shell of static safe mediocrity. - Josh Waitzkin'
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .component {
+    background: #e8e8e8;
+    min-height: 400px;
+  }
+</style>
